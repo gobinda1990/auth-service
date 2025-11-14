a67955f542d88945e2940390a17a02dfadb3bbad
gobinda <sasmal.gobind04@gmail.com>
2025-11-14 13:12:29 +0530

    check

--- src/main/java/comtax/gov/webapp/config/SecurityConfig.java
+++ src/main/java/comtax/gov/webapp/config/SecurityConfig.java
@@ -69,11 +69,11 @@ public class SecurityConfig {
     @Bean
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "*"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedOriginPatterns(List.of("*")); // allow all origins safely
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true); // required for cookies
+        configuration.setAllowCredentials(false); // required for cookies
         configuration.setExposedHeaders(List.of("Set-Cookie", "Authorization"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
